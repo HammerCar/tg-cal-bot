2d62241a23ab3e74ec211198f4111b26db9b69da
Teemu <teemur11@gmail.com>
2024-04-11 00:53:15 +0300

    Fixed path parsing

--- src/app/api/[route]/route.ts
+++ src/app/api/[route]/route.ts
@@ -4,7 +4,7 @@ import { type NextRequest } from "next/server";
 export async function POST(request: NextRequest) {
   console.log(request.headers.get("content-type"));
 
-  const path = request.nextUrl.pathname.substring(4);
+  const path = request.nextUrl.pathname.substring(4).split("?")[0];
   if (path !== process.env.TG_TOKEN) {
     return new Response("Unauthorized", { status: 401 });
   }
